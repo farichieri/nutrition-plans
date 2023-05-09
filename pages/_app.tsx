3df437fa51a8933bb8f5c3b283cc4e9b72c41250
Fabricio Richieri <fabriciorichieri@yahoo.com>
2023-05-09 13:36:33 -0300

    progress

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,6 +16,8 @@ import type { AppProps } from "next/app";
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
 
+  NProgress.configure({ showSpinner: false });
+
   useEffect(() => {
     if (typeof window !== "undefined") {
       const handleResize = () => {
